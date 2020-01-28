aea66ebc1497d0a8a15cf430e85bd9aca6d749be
Stefan Burghelea <stefanburghelea@outlook.com>
2020-01-28 09:46:22 +0000

    notify e register tem sucesso

---
+++ app/Listeners/RegisterCustomerToNewsletter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NewCustomerRegisteredEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class RegisterCustomerToNewsletter
+{
+    /**
+     * Handle the event.
+     *
+     * @param  NewCustomerRegisteredEvent  $event
+     * @return void
+     */
+    public function handle(NewCustomerRegisteredEvent $event)
+    {
+        dump('Registered to newsletter');
+    }
+}
